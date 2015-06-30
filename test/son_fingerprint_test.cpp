a9d7aba0f0d3ace5679c8f16d3a297f92f6fd7f2
Pascal Debus <pdebus@stud.phys.ethz.ch>
2015-06-30 14:19:25 +0200

    Bugfixes fingerprint_test

--- test/son_fingerprint_test.cpp
+++ test/son_fingerprint_test.cpp
@@ -12,7 +12,7 @@
 #include "../core/visualization.hpp"
 
 
-static void arrowedLine(cv::Mat img, cv::Point pt1, cv::Point pt2, const cv::Scalar& color,
+void arrowedLine(cv::Mat img, cv::Point pt1, cv::Point pt2, const cv::Scalar& color,
 	int thickness=1, int line_type=8, int shift=0, double tipLength=0.1)
 {
     const double tipSize = norm(pt1-pt2)*tipLength; // Factor to normalize the size of the tip depending on the length of the arrow
@@ -36,11 +36,14 @@ int main(int argc, const char *argv[])
 	    std::cerr << "Usage : " << argv[0] << " image" << std::endl;
 	    return 1;
 	}
+	
+	std::string fname(argv[1]);
 
 	typedef Manifold< SO, 2 > mf_t;
 	typedef Data< mf_t, 2> data_t;	
 	typedef Functional<FIRSTORDER, ISO, mf_t, data_t> func_t;
 	typedef TV_Minimizer< IRLS, func_t, mf_t, data_t, OMP > tvmin_t;
+	//typedef algo_traits< mf_t>;
 	typedef Visualization<SO, 2, data_t> visual_t;
 
 	vpp::image2d<unsigned char> img = vpp::from_opencv<unsigned char>(cv::imread(argv[1]));
@@ -72,34 +75,39 @@ int main(int argc, const char *argv[])
 
 	myData.output_matval_img("son_img1.csv");
 
-	cv::Mat original;
+	cv::Mat original, copy;
 	original = cv::imread(argv[1]);
-	
+	copy = original.clone();
+
 	vpp::image2d<double> angle_with_border = vpp::clone(angles, vpp::_border=5);
 	auto BN = vpp::box_nbh2d<double, 5, 5>(angle_with_border);
-	vpp::pixel_wise(angle_with_border.domain(), BN) | [&] (const auto& d, const auto& n){
-	    if(d(0)%10==0 && d(1)%10==0){
+    
+	auto draw_vector =  [&] (const auto& d, const auto& n){
+	   int spacing = 12; 
+	    if(d(0) % spacing == 0 && d(1) % spacing == 0){
 		double avg = 0;
 		
 		for(int i=-2; i<=2; ++i)
 		    for(int j=-2; j<=2; ++j)
 			avg +=n(i,j);
 		avg/=25.0;
-		
-		double length = 6.0;
-
-		cv::Point source(d(0),d(1));
-		cv::Point target(d(0) + length * std::cos(avg), d(1) + length * std::sin(avg));
-		arrowedLine(original, source, target, cv::Scalar( 0, 255, 0 ) );
-	    }
-	};
 	
+		double length = 7.0;
+
+		cv::Point source(d(1),d(0));
+		cv::Point target(d(1) + length * std::cos(avg), d(0) + length * std::sin(avg));
+		arrowedLine(original, source, target, cv::Scalar( 0, 255, 0 ), 1, 8, 0, 0.35 );
+	    }
+	};	
+	
+	
+	vpp::pixel_wise(angle_with_border.domain(), BN) | draw_vector;
 
 	cv::namedWindow( "Input Picture", cv::WINDOW_NORMAL ); 
 	cv::imshow( "Input Picture", original);
+	cv::imwrite("input_" + fname, original);
 	cv::waitKey(0);
 
-/*
 	double lam=0.1;
 	func_t myFunc(lam, myData);
 	myFunc.seteps2(1e-10);
@@ -107,13 +115,22 @@ int main(int argc, const char *argv[])
 	tvmin_t myTVMin(myFunc, myData);
 
 	std::cout << "Smoothen picture to obtain initial state for Newton iteration..." << std::endl;
-	myTVMin.smoothening(1);
+	myTVMin.smoothening(5);
 
 	std::cout << "Start TV minimization..." << std::endl;
 	myTVMin.minimize();
-*/
+    
+	vpp::pixel_wise(myData.img_, angle_with_border) | [&] (const auto& i, auto& a) {
+	    a = std::acos(i(0,0));
+	};
+ 
+	original = copy.clone();
+	vpp::pixel_wise(angle_with_border.domain(), BN) | draw_vector;
 
-
+	cv::namedWindow( "Denoised Picture", cv::WINDOW_NORMAL ); 
+	cv::imshow( "Denoised Picture", original);
+	cv::imwrite("denoised_" + fname, original);
+	cv::waitKey(0);
 
 
 
